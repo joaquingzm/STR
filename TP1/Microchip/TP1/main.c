173eef03680c2416fc6cf156aa7e17170ff76496
Joaquin <joaquin.gzm234@gmail.com>
2026-09-02 14:30:45 -0300

    Organizando el repo

---
+++ TP1/Microchip/TP1/main.c
@@ -0,0 +1,18 @@
+/*
+ * TP1.c
+ *
+ * Created: 9/2/2026 14:24:56
+ * Author : joaqu
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ TP2/Microchip/TP2/main.c
@@ -0,0 +1,18 @@
+/*
+ * TP2.c
+ *
+ * Created: 9/2/2026 14:26:45
+ * Author : joaqu
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
