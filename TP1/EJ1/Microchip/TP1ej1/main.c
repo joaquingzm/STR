23c928813f80b409e1c620cb80eb1b178edffa15
Joaquin <joaquin.gzm234@gmail.com>
2026-09-08 15:37:52 -0300

    Arreglo de ramas

--- TP1/EJ1/Microchip/TP1ej1/main.c
+++ TP1/EJ1/Microchip/TP1ej1/main.c
@@ -1,4 +1,4 @@
-#define F_CPU 16000000UL 
+#define F_CPU 16000000UL
 
 #include <avr/io.h>
 #include <util/delay.h>
@@ -20,25 +20,25 @@ int main(void) {
 				modo_parpadeo = true; // se detectó presión
 			}
 		}
-if (modo_parpadeo) {
-	// Estado 1: Forzar PB0 en HIGH y PB1 en LOW de forma directa
-	PORTB = (PORTB & ~((1 << PORTB0) | (1 << PORTB1))) | (1 << PORTB0);
-	_delay_ms(250);
+		if (modo_parpadeo) {
+			// Estado 1: Forzar PB0 en HIGH y PB1 en LOW de forma directa
+			PORTB = (PORTB & ~((1 << PORTB0) | (1 << PORTB1))) | (1 << PORTB0);
+			_delay_ms(250);
 
-	// Estado 2: Forzar PB0 en LOW y PB1 en HIGH de forma directa
-	PORTB = (PORTB & ~((1 << PORTB0) | (1 << PORTB1))) | (1 << PORTB1);
-	_delay_ms(250);
-}
+			// Estado 2: Forzar PB0 en LOW y PB1 en HIGH de forma directa
+			PORTB = (PORTB & ~((1 << PORTB0) | (1 << PORTB1))) | (1 << PORTB1);
+			_delay_ms(250);
+		}
 	}
 }
 
 // Configuración completa del hardware
 void setup_hardware(void) {
-	// PB0 y PB1 como salidas 
+	// PB0 y PB1 como salidas
 	DDRB |= (1 << DDB0) | (1 << DDB1);
 	// PD2 y PD3 como entradas
 	DDRD &= ~((1 << DDD2) | (1 << DDD3));
-	// resistencias Pull-Up internas en PD2 y PD3 
+	// resistencias Pull-Up internas en PD2 y PD3
 	PORTD |= (1 << PORTD2) | (1 << PORTD3);
 	// ambos LEDs totalmente encendidos al iniciar
 	PORTB |= (1 << PORTB0) | (1 << PORTB1);
@@ -47,7 +47,7 @@ void setup_hardware(void) {
 // lectura de botones
 bool boton_presionado(uint8_t pin_bit) {
 	if (!(PIND & (1 << pin_bit))) {      // presionado
-		_delay_ms(20);                 
+		_delay_ms(20);
 		if (!(PIND & (1 << pin_bit))) {  // rechequeo del estado
 			return true;
 		}
